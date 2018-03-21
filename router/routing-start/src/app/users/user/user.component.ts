4ecef1b4fa31decf4e4d38ef27aa34866c0634b7
rajesh23-drg <rmanjunath@teamdrg.com>
2018-03-21 09:45:17 +0530

    An important note oabout route observables (using onDestroy to unsbscribe)

--- router/routing-start/src/app/users/user/user.component.ts
+++ router/routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+
+// to handle destorying of subscription's
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+
+// angular handles destroying of this subscription automatically for route observables, if not we would have needed to handle it
+
+// export class UserComponent implements OnInit {
+//   user: {id: number, name: string};
+//
+//   constructor(private route: ActivatedRoute) { }
+//
+//   ngOnInit() {
+//     this.user = {
+//       id: this.route.snapshot.params['id'],
+//       name: this.route.snapshot.params['name']
+//     }
+//     // when ever reloading the same component or page instead to re-intialise the component which is aperformance issue
+//     // this approach is better, params is an observable
+//     this.route.params
+//               .subscribe(
+//                 (params: Params) => {
+//                   // when ever params changes this function executes
+//                   this.user.id = params['id'];
+//                   this.user.name = params['name'];
+//                  },
+//               );
+//   }
+//}
+
+
+// code manually handing cleaning of subscription's
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,8 +50,8 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     }
     // when ever reloading the same component or page instead to re-intialise the component which is aperformance issue
-    // this approach is better
-    this.route.params
+    // this approach is better, params is an observable
+    this.paramsSubscription = this.route.params
               .subscribe(
                 (params: Params) => {
                   // when ever params changes this function executes
@@ -26,6 +59,10 @@ export class UserComponent implements OnInit {
                   this.user.name = params['name'];
                  },
               );
+  }
+
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe();
   }
 
 }
