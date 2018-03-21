6f8aa5937879648808c316a280cdf5ba43849592
rajesh23-drg <rmanjunath@teamdrg.com>
2018-03-21 10:01:05 +0530

    retriving query params and fragments

--- router/routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ router/routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
@@ -12,9 +14,18 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // this also has same issue oly when component is created params are passed, if accessed from same page need
+    // observable
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+
+    // in that case when on same page accesse, angular handles unsubscriptions here
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
