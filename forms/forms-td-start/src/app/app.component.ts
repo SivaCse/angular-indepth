c2f89fc680f69c54bd2a7bc8aa9f3b8f42ebf28c
rajesh23-drg <rmanjunath@teamdrg.com>
2018-04-08 13:00:49 +0530

    setting and patching Form Values

--- forms/forms-td-start/src/app/app.component.ts
+++ forms/forms-td-start/src/app/app.component.ts
@@ -13,8 +13,34 @@ export class AppComponent {
   answer='';
   genders=['male','female'];
 
+  // we set values using two way binding, there is another way to do using the form here
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this below will set the value of complete form
+    // this.signUpForm.setValue({});
+
+
+    // 1st approach => not better coz once again click on the button will clear all fields
+    // this.signUpForm.setValue({
+    //   // console log the form and copy paste the structure
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    //2nd approach to override specific attrs
+    // setValue => override whole form
+    // patchValue => olu specific parts of form
+    // patchValue oly available on form, even setValue is available on this
+    this.signUpForm.form.patchValue({
+      userData: {
+          username: suggestedName
+        },
+    })
   }
 
   // // 1st approach
