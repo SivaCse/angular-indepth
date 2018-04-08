5985236f7306432ee4711c20d7cd764a01b7cfc0
rajesh23-drg <rmanjunath@teamdrg.com>
2018-04-08 16:54:21 +0530

    Reactive: Adding Validation

--- forms/forms-reactive-start/src/app/app.component.ts
+++ forms/forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +14,8 @@ export class AppComponent implements OnInit {
     // build this as per the structure in template
     this.signUpForm = new FormGroup({
       // to avoid issues during minification and since this is used in htl wrap it in ''
-      'username': new FormControl(null),
-      'email': new FormControl(null),
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male')
     });
   }
