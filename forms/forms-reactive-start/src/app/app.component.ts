dd7e0baaaba1932b8676a5dd551efcca9f0f1d5a
rajesh23-drg <rmanjunath@teamdrg.com>
2018-04-09 08:36:07 +0530

    Reactive: creating custom validators

--- forms/forms-reactive-start/src/app/app.component.ts
+++ forms/forms-reactive-start/src/app/app.component.ts
@@ -9,13 +9,15 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signUpForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit(){
     // build this as per the structure in template
     this.signUpForm = new FormGroup({
       // to avoid issues during minification and since this is used in html wrap it in ''
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        // the reason we bind(this) here is coz, this code will be executed by angular and will no know about this
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -31,4 +33,13 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    // null or nothing should be returned not {'nameIsForbidden': false}
+    return null;
+  }
+
 }
